aabb9d8d4d27ae63facb513cdd6893a23c5baf0e
Gerard Alberola <gealber@alumni.uv.es>
2020-10-20 15:55:57 +0200

    Creación de las Clases Cliente.java y Servidor.java

--- Proyecto-ARC/src/proyecto/arc/Cliente.java
+++ Proyecto-ARC/src/proyecto/arc/Cliente.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package proyecto.arc;
+import java.io.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -11,4 +16,30 @@ package proyecto.arc;
  */
 public class Cliente {
     
+    public static void main(String[] args) throws IOException
+    {
+        final String HOST = "127.0.0.1";
+        final int PUERTO = 10578;
+        DataInputStream dis;
+        DataOutputStream dos;
+        
+        try
+        {
+            Socket sc = new Socket(HOST, PUERTO);
+            dis = new DataInputStream(sc.getInputStream());    
+            dos = new DataOutputStream(sc.getOutputStream());
+            
+            dos.writeUTF("Mensaje de prueba del cliente");
+            String mensajeCliente = dis.readUTF();
+            System.out.println(mensajeCliente);
+            sc.close();
+                    
+        }
+        catch(IOException ex)
+        {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
 }
--- Proyecto-ARC/src/proyecto/arc/Servidor.java
+++ Proyecto-ARC/src/proyecto/arc/Servidor.java
@@ -5,17 +5,52 @@
  */
 package proyecto.arc;
 
+import com.sun.istack.internal.logging.Logger;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import sun.util.logging.PlatformLogger;
+
 /**
  *
  * @author juan2
  */
 public class Servidor {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args) throws IOException {
+       
+        ServerSocket servidor = null;
+        Socket sc = null;
+        DataInputStream dis;
+        DataOutputStream dos;
+        final int PUERTO = 10578;
+        
+        try
+        {
+            servidor = new ServerSocket(PUERTO);
+            System.out.println("Se ha establecido la conexion con el servidor");
+            
+            while(true)
+            {
+                sc = servidor.accept();
+                dis = new DataInputStream(sc.getInputStream());
+                dos = new DataOutputStream(sc.getOutputStream());
+                String mensajePrueba = dis.readUTF();
+                System.out.println(mensajePrueba);
+                dos.writeUTF("La conexion con el servidor ha sido correcta");
+                sc.close();
+                System.out.println("El cliente se ha desconectado del servidor");
+            }
+        }
+        catch(IOException ex)
+        {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+              
     }
     
 }
