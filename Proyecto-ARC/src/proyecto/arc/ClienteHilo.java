b2e773efe62e09cbec209656b65ce57eb1e69520
anina <anina@10.20.36.126>
2020-10-22 15:33:36 +0200

    cambio de un sout tonto para que muestre el identificador de cual se ha despertado

--- Proyecto-ARC/src/proyecto/arc/ClienteHilo.java
+++ Proyecto-ARC/src/proyecto/arc/ClienteHilo.java
@@ -35,7 +35,7 @@ public class ClienteHilo extends Thread{
     public void run(){
         try {
             //Aqui poner las instrucciones que hara el hilo del cliente - jx
-            System.out.println("hola me han despertado");
+            System.out.println("Soy el cliente " + id +" me han despertado");
              
            
             
